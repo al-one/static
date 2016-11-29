29504534ff9709d32e8d06c31fe0777d235ec145
Alone <Alone@an56.net>
2016-11-29 11:55:37 +0800

    add fanyi.baidu.js 201611291155

--- js/fanyi.baidu.js
+++ js/fanyi.baidu.js
@@ -43,24 +43,66 @@ function($)
 {
 
   var box = $('.anfy-box'),
+      top = 0,
+      lft = 0,
+      pre = null,
+      trans = function(txt)
+      {
+        getapi(
+        {
+          url:'http://fanyi.baidu.com/transapi?' + $.param({from:'auto',to:'auto',source:'txt',query:txt}),
+          retType:'json2jsonp',
+          dataType:'json'
+        })
+        .done(function(data,textStatus,jqXHR)
+        {
+          var dat = data || {},
+              dls = dat.data || [],
+              res = typeof dat.result === 'object' ? dat.result : $.parseJSON(dat.result || '{}');
+          console.log(res,dls);
+          box.find('.anfy-hd .anfy-ipt').val(res.src || (dls[0] || {}).src || '');
+          box.find('.anfy-bd').html('<ul></ul>');
+          if(dls[0])
+          {
+            $('<li>' + dls[0].dst + '</li>').appendTo(box.find('.anfy-bd ul'));
+          }
+          if(res.content) $.each(res.content || [],function(i,v)
+          {
+            $.each(v.mean || [],function(mi,mv)
+            {
+              var rls = ['<li>'];
+              rls.push(mv.pre || '');
+              rls.push(' ');
+              rls.push(Object.keys(mv.cont || {}).join('; '));
+              rls.push('</li>');
+              $(rls.join('')).appendTo(box.find('.anfy-bd ul'));
+            });
+          });
+          box.stop(true,true).show();
+        })
+        .fail(function(jqXHR,textStatus,errorThrown)
+        {
+          console.log('error',argument);
+        });
+      },
       getapi = function(url,cfg)
-  {
-    if(typeof url === 'object')
-    {
-      cfg = url;
-      url = cfg.url || '';
-    }
-    cfg || (cfg = {});
-    cfg.dataType || (cfg.dataType = 'jsonp');
-    var typ = cfg.retType || 'json';
-    cfg.url = 'http://getapi.sinaapp.com/http/?callback=?&type=' + typ + '&url=' + encodeURIComponent(url);
-    if(cfg.data)
-    {
-      cfg.url += '&data=' + encodeURIComponent(typeof cfg.data === 'object' ? $.param(cfg.data) : cfg.data);
-      delete cfg.data;
-    }
-    return $.ajax(cfg);
-  };
+      {
+        if(typeof url === 'object')
+        {
+          cfg = url;
+          url = cfg.url || '';
+        }
+        cfg || (cfg = {});
+        cfg.dataType || (cfg.dataType = 'jsonp');
+        var typ = cfg.retType || 'json';
+        cfg.url = 'http://getapi.sinaapp.com/http/?callback=?&type=' + typ + '&url=' + encodeURIComponent(url);
+        if(cfg.data)
+        {
+          cfg.url += '&data=' + encodeURIComponent(typeof cfg.data === 'object' ? $.param(cfg.data) : cfg.data);
+          delete cfg.data;
+        }
+        return $.ajax(cfg);
+      };
 
   $('body')
   .on('mouseup',function(evt)
@@ -68,49 +110,37 @@ function($)
     var the = $(this),
         sel = window.getSelection(),
         txt = $.trim(sel.toString()),
-        ost = the.offset(),
-        lft = evt.pageX - ost.left,
-        top = evt.pageY - ost.top;
-    if(txt.length > 1)
+        ost = the.offset();
+    if($(evt.target).closest('.anfy-box').length) return false;
+    lft = evt.pageX - ost.left;
+    top = evt.pageY - ost.top;
+    /\s/.test(txt) || (txt = txt.replace(/(.)(?=[A-Z][a-z]+)/g,'$1 ').replace(/([^A-Z])(?=[A-Z]{2,})/g,'$1 '));
+    if(txt)
     {
-      getapi(
+      if(txt != pre)
       {
-        url:'http://fanyi.baidu.com/transapi?' + $.param({from:'auto',to:'auto',source:'txt',query:txt}),
-        retType:'json2jsonp',
-        dataType:'json'
-      })
-      .done(function(data,textStatus,jqXHR)
+        box.stop(true,true).css({top:top + 16,left:lft,width:'auto',height:'auto'});
+        trans(txt);
+        pre = txt;
+      }
+      else setTimeout(function()
       {
-        var dat = data || {},
-            dls = dat.data || [],
-            res = typeof dat.result === 'object' ? dat.result : $.parseJSON(dat.result || '{}');
-        console.log(res,dls);
-        box.find('.anfy-hd').text(res.src || (dls[0] || {}).src || '');
-        box.find('.anfy-bd').html('<ul></ul>');
-        if(dls[0])
-        {
-          $('<li>' + dls[0].dst + '</li>').appendTo(box.find('.anfy-bd ul'));
-        }
-        if(res.content) $.each(res.content || [],function(i,v)
-        {
-          $.each(v.mean || [],function(mi,mv)
-          {
-            var rls = ['<li>'];
-            rls.push(mv.pre || '');
-            rls.push(' ');
-            rls.push(Object.keys(mv.cont || {}).join('; '));
-            rls.push('</li>');
-            $(rls.join('')).appendTo(box.find('.anfy-bd ul'));
-          });
-        });
-        box.stop().css({top:top + 14,left:lft}).show();
-      })
-      .fail(function(jqXHR,textStatus,errorThrown)
-      {
-        console.log('error',argument);
-      });
+        box.stop(true,true).show();
+      },500);
     }
   })
+  .on('keyup','.anfy-ipt',function(evt)
+  {
+    var the = $(this),
+        txt = the.val();
+    if(txt && evt.keyCode == 13)
+    {
+      trans(txt);
+    }
+    return false;
+  });
+
+  $(document)
   .on('click',function(evt)
   {
     if(!$(evt.target).closest('.anfy-box').length) box.hide(100);
@@ -118,7 +148,7 @@ function($)
 
   if(!box.length)
   {
-    box = $('<div class="anfy-box"><div class="anfy-hd"></div><div class="anfy-bd"></div></div>').appendTo('body');
-    $('head').append('<style>.anfy-box { position:absolute; top:0; left:0; display:none; max-width:480px; padding:5px; border:1px solid #dcdcdc; border:1px solid rgba(0,0,0,.2); background:#fff; transition:opacity .218s; box-shadow:0 2px 4px rgba(0,0,0,.2); font-size:12px; line-height:20px; z-index:1; } .anfy-box ul { margin:0; padding-left:20px; }</style>');
+    box = $('<div class="anfy-box"><div class="anfy-hd"><input class="anfy-ipt"></div><div class="anfy-bd"></div></div>').appendTo('body');
+    $('head').append('<style>.anfy-box { position:absolute; top:0; left:0; display:none; max-width:480px; padding:5px; border:1px solid #dcdcdc; border:1px solid rgba(0,0,0,.2); background:#fff; transition:opacity .218s; box-shadow:0 2px 4px rgba(0,0,0,.2); font-size:12px; line-height:20px; z-index:1; } .anfy-box .anfy-ipt { width:100%; } .anfy-box ul { margin:0; padding-left:20px; }</style>');
   }
 });
